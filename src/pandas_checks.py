bbbcbd46981964deb66da0aab0a17ee2173eda81
PeetV <peetjvermeulen@gmail.com>
2019-11-19 21:27:36 +1100

    Examples of DataFrame and column checks implemented with unit tests

--- src/pandas_checks.py
+++ src/pandas_checks.py
@@ -3,12 +3,12 @@ from typing import Tuple
 
 # DataFrame level checks (as opposed to column level checks)
 # are described in functions using the naming convention
-# dfcheck_[some name](df: pd.DataFrame, [inputs]) -> Tuple[bool, str]
+# dfcheck_[some name](df: pd.DataFrame, [inputs]) -> Tuple[bool, str].
 # The return value is a tuple containing a bool indicating if the
 # test passed or failed (true = passed) and a string describing
 # the error condition if the test failed. Error messages follow
 # the pattern 'want [expected value or condition], got [actual
-# value or condition'.
+# value or condition]'.
 
 def dfcheck_min_rows(df: pd.DataFrame, min_rows: int) -> Tuple[bool, str]:
     """Check if a DataFrame has a minimum number of rows."""
@@ -16,3 +16,31 @@ def dfcheck_min_rows(df: pd.DataFrame, min_rows: int) -> Tuple[bool, str]:
     if num_rows >= min_rows:
         return True, ''
     return False, f'want {min_rows} rows, got {num_rows}'
+
+def dfcheck_no_duplicate_rows(df: pd.DataFrame) -> Tuple[bool, str]:
+    """Check if a DataFrame has duplicate rows."""
+    num_duplicates: int = sum(df.duplicated())
+    if num_duplicates == 0:
+        return True, ''
+    return False, f'want 0 duplicate rows, got {num_duplicates}'
+
+# DataFrame column level checks are described in functions using
+# the naming convention colcheck_[some name](df: pd.DatFrame,
+# col_name: str, [inputs]) -> Tuple[bool, str].
+# The return value is a tuple containing a bool indicating if the
+# test passed or failed (true = passed) and a string describing
+# the error condition if the test failed. Error messages follow
+# the pattern 'want [expected value or condition], got [actual
+# value or condition]'.
+
+def colcheck_min_val(df: pd.DataFrame, col_name: str, min_val: float) -> Tuple[bool, str]:
+    """Check if a column has a value less than a minimum value."""
+    if not col_name in df.columns:
+        return False, f'column name {col_name} not found in data'
+    if not pd.api.types.is_numeric_dtype(df[col_name]):
+        return False, f'cannot check minimum value of a non-numeric column'
+    actual_min = df[col_name].min()
+    if actual_min >= min_val:
+        return True, ''
+    return False, f'want {min_val} minimum value, got {actual_min}'
+
--- tests/pandas_checks_tests.py
+++ tests/pandas_checks_tests.py
@@ -8,6 +8,7 @@ class TestDFChecks(unittest.TestCase):
 
     def setUp(self):
         self.df_file1 = pd.read_csv('data/file1.csv')
+        self.df_file2 = pd.read_csv('data/file2.csv')
 
     def test_min_rows(self):
         passed, message = pc.dfcheck_min_rows(self.df_file1, 5)
@@ -16,6 +17,33 @@ class TestDFChecks(unittest.TestCase):
         passed, message = pc.dfcheck_min_rows(self.df_file1, 10)
         self.assertEqual(passed, False)
         self.assertEqual(message, 'want 10 rows, got 5')
+
+    def test_no_duplicate_rows(self):
+        passed, message = pc.dfcheck_no_duplicate_rows(self.df_file1)
+        self.assertEqual(passed, True)
+        self.assertEqual(message, '')
+        passed, message = pc.dfcheck_no_duplicate_rows(self.df_file2)
+        self.assertEqual(passed, False)
+        self.assertEqual(message, 'want 0 duplicate rows, got 2')
+
+class TestColChecks(unittest.TestCase):
+
+    def setUp(self):
+        self.df_file1 = pd.read_csv('data/file1.csv')
+
+    def test_min_val(self):
+        passed, message = pc.colcheck_min_val(self.df_file1, 'x', 1)
+        self.assertEqual(passed, False)
+        self.assertEqual(message, 'column name x not found in data')
+        passed, message = pc.colcheck_min_val(self.df_file1, 'C', 1)
+        self.assertEqual(passed, False)
+        self.assertEqual(message, 'cannot check minimum value of a non-numeric column')
+        passed, message = pc.colcheck_min_val(self.df_file1, 'A', -1)
+        self.assertEqual(passed, True)
+        self.assertEqual(message, '')
+        passed, message = pc.colcheck_min_val(self.df_file1, 'A', 2)
+        self.assertEqual(passed, False)
+        self.assertEqual(message, 'want 2 minimum value, got 1')
 
 if __name__ == '__main__':
     unittest.main()
