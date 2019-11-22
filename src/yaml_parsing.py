a922f77f2c8ff25d64f9e295726ab5e89d859014
PeetV <peetjvermeulen@gmail.com>
2019-11-22 17:10:41 +1100

    yaml dataset attributes processing

--- src/yaml_parsing.py
+++ src/yaml_parsing.py
@@ -1,10 +1,10 @@
 from yaml import load  # type: ignore
-import pandas as pd  # type: ignore
-from typing import Dict
+from typing import Dict, List
 import checksuites as cs
 
 
 _YAML_TOPLEVEL_KEYS = ['dataset', 'columns']
+_YAML_DATASET_KEYS = ['stop_on_fail', 'allow_duplicate_rows', 'min_rows']
 
 
 def load_raw_yaml(filename: str) -> Dict:
@@ -14,23 +14,39 @@ def load_raw_yaml(filename: str) -> Dict:
     return parsed
 
 
-def apply_yamldict_to_checksuite(ymld: Dict,
-                                 suite: cs.PandasDatsetCheckSuite,
-                                 df: pd.DataFrame) -> None:
-    '''Apply yaml parsed into dictionary to a checksuite object.'''
-    ykeys = ymld.keys()
+def _checktoplevelkeys(ykeys: List[str]) -> None:
     for key in ykeys:
         if key not in _YAML_TOPLEVEL_KEYS:
-            raise AttributeError(f'Unexpected yaml attribute {key}')
+            raise AttributeError(f'unexpected yaml attribute {key}')
+
+
+def _checkdatasetkeys(dsdictkeys: List[str]) -> None:
+    for key in dsdictkeys:
+        if key not in _YAML_DATASET_KEYS:
+            raise AttributeError(f'unexpected dataset attribute {key}')
+
+
+def apply_yamldict_to_checksuite(ymld: Dict,
+                                 suite: cs.PandasDatsetCheckSuite) -> None:
+    '''Apply yaml parsed into dictionary to a checksuite object.'''
+    ykeys = list(ymld.keys())
+    _checktoplevelkeys(ykeys)
     if 'dataset' in ykeys:
         dsdict = ymld['dataset']
-        dsdictkeys = dsdict.keys()
+        dsdictkeys = list(dsdict.keys())
+        _checkdatasetkeys(dsdictkeys)
         if 'stop_on_fail' in dsdictkeys:
             if dsdict['fail'] is True:
                 suite.stop_on_fail = True
         if 'allow_duplicate_rows' in dsdictkeys:
             if dsdict['allow_duplicate_rows'] is False:
                 suite.allow_duplicate_rows = False
+        if 'min_rows' in dsdictkeys:
+            val = dsdict['min_rows']
+            if not isinstance(val, int):
+                raise AttributeError((f'dataset: min_rows want an integer, '
+                                      f'got {val}({type(val)})'))
+            suite.min_rows = val
 
 
 if __name__ == '__main__':
