95ee92b4f6c7fe857a771279fe59efd8174a2f3a
PeetV <peetjvermeulen@gmail.com>
2019-11-24 19:42:25 +1100

    more Pythonic run all tests

--- tests/runalltests.sh
+++ tests/runalltests.sh
@@ -1,12 +1,2 @@
 #!/bin/zsh
-echo "pandas_checks_tests.py"
-python3 pandas_checks_tests.py
-
-echo "\nchecksuites_tests.py"
-python3 checksuites_tests.py
-
-echo "\nyaml_parsing_tests.py"
-python3 yaml_parsing_tests.py
-
-echo "\nwhistle_tests.py"
-python3 whistle_tests.py
+python3 -m unittest pandas_checks_tests checksuites_tests yaml_parsing_tests whistle_tests
