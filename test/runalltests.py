5b77942ba97c2e71eec252837b933776d8e94b89
PeetV <peetjvermeulen@gmail.com>
2019-11-29 17:24:31 +1100

    Relative path imports for running all tests

---
+++ test/runalltests.py
@@ -0,0 +1,7 @@
+import inspect, os, unittest
+HDIR = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+
+loader = unittest.TestLoader()
+suite = loader.discover(HDIR)
+runner = unittest.TextTestRunner()
+runner.run(suite)
